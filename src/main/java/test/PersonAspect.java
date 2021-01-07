3ae93ffdccbdabbbe09438b287bb53ac5426a1d4
TOM <2770573198@qq.com>
2021-01-07 13:15:11 +0800

    新增测试代码

--- src/main/java/test/PersonAspect.java
+++ src/main/java/test/PersonAspect.java
@@ -1,7 +1,9 @@
 package test;
 
+import aop.annotation.After;
 import aop.annotation.Aspect;
 import aop.annotation.Before;
+import aop.annotation.PointCut;
 import aop.join_point.JoinPoint;
 
 import java.lang.reflect.Method;
@@ -9,10 +11,22 @@ import java.lang.reflect.Method;
 @Aspect
 public class PersonAspect {
 
-    @Before("execution(public .* test.Person.*())")
-    public void log(JoinPoint jp){
+    @PointCut("execution(public .* test.Person.*())")
+    public void pointCut(){
+
+    }
+
+
+    @Before("pointCut")
+    public void before(JoinPoint jp){
         Method method = jp.getMethod();
         System.out.println(method.getDeclaringClass().getName()+"中的"+method.getName()+"开始执行");
+    }
+
+    @After("pointCut")
+    public void after(JoinPoint jp){
+        Method method = jp.getMethod();
+        System.out.println(method.getDeclaringClass().getName()+"中的"+method.getName()+"执行结束");
     }
 
 }
--- src/main/java/test/TestMain.java
+++ src/main/java/test/TestMain.java
@@ -1,5 +1,12 @@
 package test;
 
+import aop.aspect.parser.AnnotationAspectDefinitionParser;
+import aop.aspect.parser.AspectDefinitionParser;
+import aop.proxy.AopProxyFactory;
+import aop.proxy.CglibAopProxyFactory;
+import aop.proxy.method.factory.MethodProxyFactory;
+import aop.proxy.method.factory.TargetMethodProxyFactory;
+
 /**
  * @author lijia
  * @description
@@ -8,6 +15,20 @@ package test;
 public class TestMain {
 
     public static void main(String[] args) {
+        //通过new创建的对象
+        Person person = new Person();
 
+        //创建切面信息解析器
+        AspectDefinitionParser parser = new AnnotationAspectDefinitionParser("test");
+
+        //创建方法代理工厂
+        MethodProxyFactory methodProxyFactory = new TargetMethodProxyFactory(parser);
+
+        //创建对象代理工厂
+        AopProxyFactory proxyFactory = new CglibAopProxyFactory(methodProxyFactory);
+
+        Person proxy = (Person) proxyFactory.getProxy(person);
+
+        proxy.eat();
     }
 }
